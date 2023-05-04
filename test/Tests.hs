1d5e3e044044b336a06090de9a4afe97fd471e3f
harris-chris <chrisharriscjh@gmail.com>
2023-05-04 11:13:32 +0900

    First test passing

--- test/Tests.hs
+++ test/Tests.hs
@@ -4,17 +4,41 @@ import Test.Hspec
 
 import Debug.Trace (trace, traceShowId)
 import System.FilePath
+import System.Directory.Tree
 
+import AST
 import TreeFilter
 
 testData :: FilePath
 testData = "test/test-data"
 
+treeA :: DirTree ()
+treeA = Dir "a" [
+    File "file_a_1.cpp" ()
+    , File "file_a_2.hpp" ()
+    ]
+
+treeB :: DirTree ()
+treeB = Dir "b" [
+    File "file_b_1.cpp" ()
+    , File "file_b_2.hpp" ()
+    ]
+
+treeC :: DirTree ()
+treeC = Dir "c" [ ]
+
+treeTestData :: DirTree ()
+treeTestData = Dir "test-data" [ treeA, treeB, treeC ]
+
+compareToExpected :: DirTree () -> DirTree FsObjData -> IO ()
+compareToExpected expected actual =
+    let actual' = () <$ actual
+    in actual' `shouldBe` expected
+
 main :: IO ()
 main = hspec $ do
     describe "filterTree" $ do
         it "Correctly reproduces a tree if include is used" $ do
-            let origTree = getTree testData
-            let filteredTree = filterTree "include" origTree
-            origTree `shouldBe` filteredTree
+            let expected = treeTestData
+            applyFilterWith "./test-data" "include" ( compareToExpected expected )
 
